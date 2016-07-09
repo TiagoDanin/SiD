5305902f1be0754f58f23d78e7aaab4b8e97f9e6
Tiago Danin <tiagojdanin@hotmail.com>
2016-07-09 10:50:00 -0500

    FIX e Update Time

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,30 +1,35 @@
 local command = 'time <$location*>'
-local doc = [[*Utilize os seguite comandos citados abaixo:*
-*________________________*
-`/horas [`*local*`]` - Para o bot informar que horas são em sua cidade ou local mencionado.]]
+local doc = [[```
+/time <$location*>
+$doc_time*.
+```]]
+
+local triggers = {
+	'^/time[@'..bot.username..']*',
+	'^/hora[@'..bot.username..']*',
+	'^/date[@'..bot.username..']*',
+	'^/data[@'..bot.username..']*'
+}
 
 local action = function(msg)
+
 	local input = msg.text:input()
 	if not input then
 		if msg.reply_to_message and msg.reply_to_message.text then
 			input = msg.reply_to_message.text
 		else
-			sendMessage(msg.chat.id, [[
-*Utilize os seguite comandos citados abaixo:*
-*________________________*
-`/horas [`*local*`]` - Para o bot informar que horas são em sua cidade ou local mencionado.
-]], true, msg.message_id, true)
+			sendMessage(msg.chat.id, sendLang(doc, lang), true, msg.message_id, true)
 			return
 		end
 	end
 
 	local coords = get_coords(input)
 	if type(coords) == 'string' then
-		api.sendReply(msg, coords)
+		sendReply(msg, coords)
 		return
 	end
 
-	local url = 'https://maps.googleapis.com/maps/api/timezone/json?location=' .. coords.lat ..','.. coords.lon .. '&timestamp='..os.time()
+	local url = 'https://maps.googleapis.com/maps/api/timezone/json?location=' .. coords.lat ..','.. coords.lon .. '&timestamp='..os.time(os.date('!*t'))
 
 	local jstr, res = HTTPS.request(url)
 	if res ~= 200 then
@@ -33,20 +38,21 @@ local action = function(msg)
 	end
 
 	local jdat = JSON.decode(jstr)
-	local timestamp = os.time(os.date("!*t"))
-    local localTime = timestamp + jdat.rawOffset + jdat.dstOffset
-	sendReply(msg, os.date('Pelo meu relogio em '..input..' agora são: %H:%M:%S %p', localTime))
+
+	local timestamp = os.time(os.date('!*t')) + jdat.rawOffset + jdat.dstOffset + config.time_offset
+	local utcoff = (jdat.rawOffset + jdat.dstOffset) / 3600
+	if utcoff == math.abs(utcoff) then
+		utcoff = '+' .. utcoff
+	end
+	local message = os.date('%I:%M %p\n', timestamp) .. os.date('%A, %B %d, %Y\n', timestamp) .. jdat.timeZoneName .. ' (UTC' .. utcoff .. ')'
+
+	sendReply(msg, message)
 
 end
 
 return {
 	action = action,
+	triggers = triggers,
 	doc = doc,
-	command = command,
-	triggers = {
-	'^/time[@'..bot.username..']*',
-	'^/data[@'..bot.username..']*',
-	'^/hora[@'..bot.username..']*'
-	}
+	command = command
 }
-
